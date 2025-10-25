79e45a414a317b2e75a4f1b00cfc336aad80edf1
Nameless <pintokevin2002@hotmail.com>
2025-10-26 00:16:22 +0200

    Creating the register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../components/RegisterForm.jsx";
+
+const Register = () => {
+    return (
+        <main className="flex justify-center items-center">
+            <RegisterForm />
+        </main>
+    );
+};
+
+export default Register;

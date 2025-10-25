bdfcfd2b97f6ef9119e4698013e2796d40ac80ae
Nameless <pintokevin2002@hotmail.com>
2025-10-26 00:16:39 +0200

    Adding routes to the login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
 const App = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
                 <Route path="*" element={<ErrorPage />} />
             </Route>
         </Routes>

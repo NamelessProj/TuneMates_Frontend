3b8904edec98ae7cf01b5c1203bb558183cf48a2
Nameless <pintokevin2002@hotmail.com>
2025-10-26 00:16:17 +0200

    Creating the login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import LoginForm from "../components/LoginForm.jsx";
+
+const Login = () => {
+    return (
+        <main className="flex justify-center items-center">
+            <LoginForm />
+        </main>
+    );
+};
+
+export default Login;

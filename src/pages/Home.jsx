ab537a1bdf3da4cacb5dd8a33a48baf8001ddcb3
Nameless <pintokevin2002@hotmail.com>
2025-10-26 15:02:50 +0100

    Adding the full form to join a room

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,31 @@
 import {Alert, Button, Card, CardBody, CardHeader, Input, Typography} from "@material-tailwind/react";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useRoomStore} from "../stores/roomStore.js";
+import NProgress from "nprogress";
+import DefaultSpinner from "../components/DefaultSpinner.jsx";
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
     const [slug, setSlug] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
-    const focusById = (id) => document.getElementById(id).focus();
+    const navigate = useNavigate();
+
+    const {room, roomError, roomLoading, getRoomBySlug} = useRoomStore();
+
+    useEffect(() => {
+        if (room) navigate(`/room/${slug}`);
+    }, [room, slug, navigate]);
+
+    const focusById = (id) => document.getElementById(id)?.focus();
 
     const handleJoinRoom = async (e) => {
         e.preventDefault();
         setError("");
 
         const roomSlug = slug.replace(/(\s+|-+)/g, "-").replace(/-+$/g, "").toLowerCase();
+        setSlug(roomSlug);
 
         if (roomSlug === "") {
             setError("Please enter a valid slug");
@@ -26,54 +39,69 @@ const Home = () => {
             return;
         }
 
-        // Send a request to get the room information
+        try {
+            NProgress.start();
+            await getRoomBySlug(roomSlug, password);
+        } finally {
+            NProgress.done();
+        }
     }
 
     return (
         <main className="flex flex-col justify-center items-center">
-            <Card className="w-[min(500px,100%)] mx-auto my-6">
-                <CardHeader floated={false} shadow={false}>
-                    <Typography variant="h5" className="text-center text-balance">
-                        Let join a room
-                    </Typography>
-                </CardHeader>
-                <CardBody>
-                    {error && (
-                        <Alert className="mb-3" color="red">
-                            {error}
-                        </Alert>
-                    )}
-                    <form className="flex flex-col" onSubmit={handleJoinRoom}>
-                        <Input
-                            className="rounded-b-none"
-                            value={slug}
-                            onChange={(e) => setSlug(e.target.value.toLowerCase().trimStart().replace(/\s+/g, '-'))}
-                            name="slug"
-                            id="slug"
-                            label="Enter the room slug"
-                            required
-                        />
-                        <Input
-                            className="rounded-none"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            name="password"
-                            id="password"
-                            label="Enter the room's password"
-                            variant="outlined"
-                            required
-                        />
-                        <Button
-                            variant="gradient"
-                            color="green"
-                            className="rounded-t-none"
-                            onClick={handleJoinRoom}
-                        >
-                            Join Room
-                        </Button>
-                    </form>
-                </CardBody>
-            </Card>
+            {roomLoading ? (
+                <DefaultSpinner />
+            ) : (
+                <Card className="w-[min(500px,100%)] mx-auto my-6">
+                    <CardHeader floated={false} shadow={false}>
+                        <Typography variant="h5" className="text-center text-balance">
+                            Let join a room
+                        </Typography>
+                    </CardHeader>
+                    <CardBody>
+                        {roomError && (
+                            <Alert className="mb-3" color="red">
+                                {roomError}
+                            </Alert>
+                        )}
+                        {error && (
+                            <Alert className="mb-3" color="red">
+                                {error}
+                            </Alert>
+                        )}
+                        <form className="flex flex-col" onSubmit={handleJoinRoom}>
+                            <Input
+                                className="rounded-b-none"
+                                value={slug}
+                                onChange={(e) => setSlug(e.target.value.toLowerCase().trimStart().replace(/\s+/g, '-'))}
+                                name="slug"
+                                id="slug"
+                                label="Enter the room slug"
+                                required
+                            />
+                            <Input
+                                className="rounded-none"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                name="password"
+                                id="password"
+                                label="Enter the room's password"
+                                variant="outlined"
+                                required
+                            />
+                            <Button
+                                variant="gradient"
+                                color="green"
+                                className="rounded-t-none"
+                                type="submit"
+                                onClick={handleJoinRoom}
+                            >
+                                Join Room
+                            </Button>
+                        </form>
+                    </CardBody>
+                </Card>
+            )}
         </main>
     );
 };

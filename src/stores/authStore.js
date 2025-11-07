641b9b9445013be9e5a2da0eefd83bd1d9483972
Nameless <pintokevin2002@hotmail.com>
2025-11-07 15:16:55 +0100

    Add userToken state and setUserToken method to authStore for token management

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -4,14 +4,22 @@ const storageKey = 'TuneMatesUserInfo';
 
 export const useAuthStore = create((set) => ({
     userInfo: localStorage.getItem(storageKey) ? JSON.parse(localStorage.getItem(storageKey)) : null,
+    userToken: localStorage.getItem(storageKey.concat("Token")) ? JSON.parse(localStorage.getItem(storageKey.concat("Token"))) : null,
 
     setCredentials: (data) => {
         set(() => ({userInfo: data}));
         localStorage.setItem(storageKey, JSON.stringify(data));
     },
 
+    setUserToken: (token) => {
+        set(() => ({userToken: token}));
+        localStorage.setItem(storageKey.concat("Token"), JSON.stringify(token));
+    },
+
     logout: () => {
         set(() => ({userInfo: null}));
+        set(() => ({userToken: null}));
         localStorage.removeItem(storageKey);
+        localStorage.removeItem(storageKey.concat("Token"));
     }
 }));

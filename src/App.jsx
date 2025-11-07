08e0cc161684ab7639e2d32a8e29e4052a2ec1f4
Nameless <pintokevin2002@hotmail.com>
2025-11-07 15:38:31 +0100

    Add Rooms route to App component for room management

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ErrorPage from "./pages/ErrorPage.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Profile from "./pages/Profile.jsx";
+import Rooms from "./pages/Rooms.jsx";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path="login" element={<Login />} />
                 <Route path="register" element={<Register />} />
                 <Route path="profile" element={<Profile />} />
+                <Route path="rooms" element={<Rooms />} />
                 <Route path="*" element={<ErrorPage />} />
             </Route>
         </Routes>

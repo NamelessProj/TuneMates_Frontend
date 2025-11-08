314d04fb77595d7a7a0b7b140009a35e71fcfcaf
Nameless <pintokevin2002@hotmail.com>
2025-11-08 23:13:57 +0100

    Add delete button to RoomListEl for room deletion functionality

--- src/components/RoomListEl.jsx
+++ src/components/RoomListEl.jsx
@@ -1,16 +1,38 @@
-import {Card, Typography} from "@material-tailwind/react";
+import {Button, Card, Typography} from "@material-tailwind/react";
+import {useRoomStore} from "../stores/roomStore.js";
+import NProgress from "nprogress";
 
-const RoomListEl = ({room}) => {
+const RoomListEl = ({room, token}) => {
+    const {roomLoading, deleteRoom} = useRoomStore();
+
+    const handleDelete = async (e) => {
+        e.preventDefault();
+        if (roomLoading) return;
+        NProgress.start();
+        await deleteRoom(room.id, token);
+        NProgress.done();
+    }
+
     return (
         <Card>
-            <div>
+            <div className="grid grid-cols-[1fr-auto] gap-x-3">
                 <Typography variant="h2">
                     {room.name}
                 </Typography>
-                <Typography>
-                    {room.createdAt}
-                </Typography>
+                <div>
+                    <Button
+                        variant="gradient"
+                        color="red"
+                        disabled={roomLoading}
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </Button>
+                </div>
             </div>
+            <Typography variant="small">
+                {room.createdAt}
+            </Typography>
         </Card>
     );
 };

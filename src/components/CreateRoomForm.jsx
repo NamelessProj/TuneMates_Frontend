eca6b9017ec9d11256bcfa380f459ff5e607ebba
Nameless <pintokevin2002@hotmail.com>
2025-11-08 22:42:24 +0100

    Add CreateRoomForm component for room creation functionality

---
+++ src/components/CreateRoomForm.jsx
@@ -0,0 +1,120 @@
+import {useRoomStore} from "../stores/roomStore.js";
+import DefaultSpinner from "./DefaultSpinner.jsx";
+import {Alert, Button, Card, CardBody, CardHeader, Input, Switch, Typography} from "@material-tailwind/react";
+import {useEffect, useState} from "react";
+import PasswordInput from "./PasswordInput.jsx";
+import NProgress from "nprogress";
+import {useNavigate} from "react-router-dom";
+
+const CreateRoomForm = ({token}) => {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [isActive, setIsActive] = useState(true);
+    const [error, setError] = useState("");
+
+    const navigate = useNavigate();
+
+    const {room, roomError, roomLoading, createRoom} = useRoomStore();
+
+    useEffect(() => {
+        if (room && !roomError) navigate("/rooms");
+    }, [room, roomError, navigate]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (name.trim().length < 3 || name.trim().length > 50) {
+            setError("Name is required and must be between 3 and 50 characters");
+            return;
+        }
+
+        if (password.trim().length === 0) {
+            setError("Password is required");
+            return;
+        }
+
+        if (password !== passwordConfirm) {
+            setError("Password and Confirm Password do not match");
+            return;
+        }
+
+        NProgress.start();
+        await createRoom({
+            name: name.trim(),
+            password,
+            passwordConfirm,
+            isActive
+        }, token);
+        NProgress.done();
+    }
+
+    return (
+        <div className="w-full">
+            {roomLoading ? <DefaultSpinner /> : (
+                <Card className="!w-full max-w-md mx-auto">
+                    <CardHeader variant="gradient" color="green" className="flex flex-col gap-4 items-center py-3">
+                        <Typography variant="h2" className="text-center text-balance">
+                            Creating Room...
+                        </Typography>
+                        {name && (
+                            <Typography variant="lead" className="text-center text-balance">
+                                {name}
+                            </Typography>
+                        )}
+                    </CardHeader>
+                    <CardBody>
+                        {roomError && (
+                            <Alert color="red" className="mb-6">
+                                {roomError}
+                            </Alert>
+                        )}
+
+                        {error && (
+                            <Alert color="red" className="mb-6">
+                                {error}
+                            </Alert>
+                        )}
+
+                        <form className="flex flex-col gap-6 flex-1" onSubmit={handleSubmit}>
+                            <Input
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                label="Room Name"
+                                required
+                            />
+                            <PasswordInput
+                                password={password}
+                                setPassword={setPassword}
+                                label="Room Password"
+                            />
+                            <PasswordInput
+                                password={passwordConfirm}
+                                setPassword={setPasswordConfirm}
+                                label="Confirm Password"
+                                name="confirmPassword"
+                                id="confirmPassword"
+                            />
+                            <Switch
+                                label="Is Active"
+                                checked={isActive}
+                                onChange={() => setIsActive(!isActive)}
+                                color="green"
+                            />
+                            <Button
+                                color="green"
+                                variant="gradient"
+                                type="submit"
+                                onClick={handleSubmit}
+                            >
+                                Create Room
+                            </Button>
+                        </form>
+                    </CardBody>
+                </Card>
+            )}
+        </div>
+    );
+};
+
+export default CreateRoomForm;

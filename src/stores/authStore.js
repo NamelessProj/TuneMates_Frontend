110eb14779d882290f657b211e9563d02e32e1f8
Nameless <pintokevin2002@hotmail.com>
2025-11-13 16:21:37 +0100

    Refactor authStore to improve token storage management

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,10 +1,11 @@
 import {create} from 'zustand';
 
 const storageKey = 'TuneMatesUserInfo';
+const tokenStorageKey = 'TuneMatesUserInfoToken';
 
 export const useAuthStore = create((set) => ({
     userInfo: localStorage.getItem(storageKey) ? JSON.parse(localStorage.getItem(storageKey)) : null,
-    userToken: localStorage.getItem(storageKey.concat("Token")) ? JSON.parse(localStorage.getItem(storageKey.concat("Token"))) : null,
+    userToken: localStorage.getItem(tokenStorageKey) ? JSON.parse(localStorage.getItem(tokenStorageKey)) : null,
 
     setCredentials: (data) => {
         set(() => ({userInfo: data}));
@@ -13,13 +14,13 @@ export const useAuthStore = create((set) => ({
 
     setUserToken: (token) => {
         set(() => ({userToken: token}));
-        localStorage.setItem(storageKey.concat("Token"), JSON.stringify(token));
+        localStorage.setItem(tokenStorageKey, JSON.stringify(token));
     },
 
     logout: () => {
         set(() => ({userInfo: null}));
         set(() => ({userToken: null}));
         localStorage.removeItem(storageKey);
-        localStorage.removeItem(storageKey.concat("Token"));
+        localStorage.removeItem(tokenStorageKey);
     }
 }));

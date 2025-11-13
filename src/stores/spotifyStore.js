bd4f1f31a07e8d625b74b7757624465737325b31
Nameless <pintokevin2002@hotmail.com>
2025-11-13 16:21:53 +0100

    Refactor spotifyStore to centralize API URL management for Spotify requests

--- src/stores/spotifyStore.js
+++ src/stores/spotifyStore.js
@@ -1,6 +1,8 @@
 import {create} from "zustand";
 import axios from "axios";
 
+const baseUrl = `${import.meta.env.VITE_API_URL}/spotify`;
+
 export const useSpotifyStore = create((set) => ({
     spotifyAuthUrl: null,
     spotifyLoading: false,
@@ -9,7 +11,7 @@ export const useSpotifyStore = create((set) => ({
     fetchSpotifyAuthUrl: async () => {
         set({spotifyLoading: true, spotifyError: null});
         try {
-            const res = await axios.get(`${import.meta.env.VITE_API_URL}/spotify/oathlink`, {
+            const res = await axios.get(`${baseUrl}/oathlink`, {
                 withCredentials: true,
                 method: "GET"
             });

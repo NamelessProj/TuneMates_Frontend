2b19c40004165b66b016b13bda6101ae5401dfe1
Nameless <pintokevin2002@hotmail.com>
2025-11-15 23:42:57 +0100

    Add RoomPage component to display current room details

---
+++ src/pages/RoomPage.jsx
@@ -0,0 +1,33 @@
+import {useRoomStore} from "../stores/roomStore.js";
+import {useNavigate, useParams} from "react-router-dom";
+import {useEffect} from "react";
+import {Typography} from "@material-tailwind/react";
+
+const RoomPage = () => {
+    const navigate = useNavigate();
+    const {slug} = useParams();
+    const {room} = useRoomStore();
+
+    useEffect(() => {
+        if (!room || slug.length === 0) navigate("/");
+    }, [room, slug, navigate]);
+
+    return (
+        <>
+            {room ? (
+                <main>
+                    <div>
+                        <Typography variant="lead" className="text-center text-balance">
+                            Current Room
+                        </Typography>
+                        <Typography variant="h2" className="text-center text-balance">
+                            {room.name}
+                        </Typography>
+                    </div>
+                </main>
+            ) : null}
+        </>
+    );
+};
+
+export default RoomPage;

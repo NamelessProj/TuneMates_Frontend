c3ef93f422dd8b4ed8f17fbd338d93365062f13d
Nameless <pintokevin2002@hotmail.com>
2025-11-17 16:29:23 +0100

    Add SongItem component for displaying song details and request functionality

---
+++ src/components/SongItem.jsx
@@ -0,0 +1,47 @@
+import {Avatar, Button, Card, CardHeader, Typography} from "@material-tailwind/react";
+
+const SongItem = ({song, calcDuration}) => {
+    return (
+        <Card
+            color="transparent"
+            shadow={false}
+            className="w-full max-w-[26rem]"
+        >
+            <CardHeader
+                color="transparent"
+                floated={false}
+                shadow={false}
+                className="mx-0 flex items-center gap-4 pt-0 pb-8"
+            >
+                <Avatar
+                    size="lg"
+                    variant="rounded"
+                    src={song.albumImageUrl}
+                    alt={song.name}
+                />
+                <div className="flex w-full flex-col gap-0.5">
+                    <div className="flex items-center justify-between">
+                        <Typography variant="h5" color="blue-gray">
+                            {song.name}
+                        </Typography>
+                        <Button
+                            size="sm"
+                            color="green"
+                            variant="gradient"
+                        >
+                            Send Request
+                        </Button>
+                    </div>
+                    <Typography color="blue-gray">
+                        {song.artist.join(", ")}
+                    </Typography>
+                    <Typography variant="small" color="blue-gray">
+                        {calcDuration(song.durationMs)}
+                    </Typography>
+                </div>
+            </CardHeader>
+        </Card>
+    );
+};
+
+export default SongItem;

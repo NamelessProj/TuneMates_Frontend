5f1c9b8e123b9766788273c293c006f5ddf241c9
Pinto <pintokevin2002@hotmail.com>
2025-11-19 10:19:48 +0100

    Adding a function to get all songs with a status, default to get pending

--- src/stores/songStore.js
+++ src/stores/songStore.js
@@ -5,6 +5,7 @@ const baseUrl = `${import.meta.env.VITE_API_URL}/songs`;
 
 export const useSongStore = create((set) => ({
     currentSong: null,
+    pendingSongs: [],
     songLoading: false,
     songError: null,
 
@@ -19,6 +20,25 @@ export const useSongStore = create((set) => ({
                 });
         } catch (err) {
             set({songError: err?.response?.data?.message || err?.message || "Failed to send song to room"});
+        } finally {
+            set({songLoading: false});
+        }
+    },
+
+    getAllSongsWithStatus: async (roomId, token, status=0) => {
+        set({songLoading: true, songError: null});
+        try {
+            const res = await axios.get(`${baseUrl}/${roomId}/status/${status}`,
+                {
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    method: "GET",
+                });
+            set(() => ({pendingSongs: res.data}));
+        } catch (err) {
+            set({songError: err?.response?.data?.message || err?.message || "Failed to get songs from room"});
         } finally {
             set({songLoading: false});
         }

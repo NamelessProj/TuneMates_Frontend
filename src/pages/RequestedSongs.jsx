b99c4ed3c7c24c5d2c510fca43734ace7d14c45b
Pinto <pintokevin2002@hotmail.com>
2025-11-19 10:49:33 +0100

    Making the page to show all pending songs in a room

---
+++ src/pages/RequestedSongs.jsx
@@ -0,0 +1,84 @@
+import {useSongStore} from "../stores/songStore.js";
+import NProgress from "nprogress";
+import {useNavigate, useParams} from "react-router-dom";
+import {useAuthStore} from "../stores/authStore.js";
+import {useEffect} from "react";
+import {Typography} from "@material-tailwind/react";
+import {useRoomStore} from "../stores/roomStore.js";
+import DefaultSpinner from "../components/DefaultSpinner.jsx";
+import OwnerSongList from "../components/OwnerSongList.jsx";
+import {toast} from "react-toastify";
+
+const RequestedSongs = () => {
+    const {roomId} = useParams();
+    const {pendingSongs, songError, songLoading, getAllSongsWithStatus} = useSongStore();
+    const {room, roomError, roomLoading, getRoomById} = useRoomStore();
+    const {userInfo, userToken} = useAuthStore();
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (roomId.length === 0 || isNaN(roomId) || !userInfo || !userToken) navigate("/login");
+
+        NProgress.start();
+        getRoomById(roomId);
+        NProgress.done();
+    }, [roomId, userInfo, userToken, navigate, getRoomById]);
+
+    useEffect(() => {
+        if (roomError) navigate("/");
+    }, [roomError, navigate]);
+
+    useEffect(() => {
+        if (songError) toast(songError);
+    }, [songError]);
+
+    useEffect(() => {
+        if (!room || roomId.length === 0 || isNaN(roomId) || !userInfo || !userToken) return;
+
+        const requestPandingSongs = async () => {
+            NProgress.start();
+            const pendingStatusCode = 0;
+            await getAllSongsWithStatus(roomId, userToken, pendingStatusCode);
+            NProgress.done();
+        }
+
+        const interval = 15_000; // 15 seconds
+        const intervalFunc = setInterval(() => {
+            requestPandingSongs();
+        }, interval);
+
+        return () => {
+            clearInterval(intervalFunc);
+        }
+    }, [room, roomId, userToken, userInfo, getAllSongsWithStatus]);
+
+    return (
+        <>
+            {roomLoading ? (
+                <main className="flex justify-center items-center">
+                    <DefaultSpinner />
+                </main>
+            ) : (
+                <main>
+                    <Typography variant="lead" className="text-center text-balance">
+                        Pending songs for the room
+                    </Typography>
+                    <Typography variant="h2" className="text-center text-balance">
+                        {room.name}
+                    </Typography>
+
+                    {(songLoading && pendingSongs.length > 0) ? (
+                        <div className="flex justify-center items-center my-6">
+                            <DefaultSpinner />
+                        </div>
+                    ) : null}
+
+                    <OwnerSongList songs={pendingSongs} />
+                </main>
+            )}
+        </>
+    );
+};
+
+export default RequestedSongs;

b1c2f3e601a7836121acb02abee1adf400ec663f
Pinto <pintokevin2002@hotmail.com>
2025-11-21 09:13:42 +0100

    Adding a method to get the user's Spotify playlists

--- src/stores/spotifyStore.js
+++ src/stores/spotifyStore.js
@@ -5,6 +5,7 @@ const baseUrl = `${import.meta.env.VITE_API_URL}/spotify`;
 
 export const useSpotifyStore = create((set) => ({
     spotifyAuthUrl: null,
+    userPlaylists: null,
     spotifyLoading: false,
     spotifyError: null,
 
@@ -37,6 +38,25 @@ export const useSpotifyStore = create((set) => ({
                 });
         } catch (err) {
             set({spotifyError: err?.response?.data?.message || err?.message || "Failed to add the song to the playlist"});
+        } finally {
+            set({spotifyLoading: false});
+        }
+    },
+
+    getUserPlaylist: async (token) => {
+        set({spotifyLoading: true, spotifyError: null});
+        try {
+            const res = await axios.get(`${baseUrl}/playlist/me`,
+                {
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    },
+                    method: "GET"
+                });
+            set(() => ({userPlaylists: res.data.items}));
+        } catch (err) {
+            set({spotifyError: err?.response?.data?.message || err?.message || "Failed to load user playlists"});
         } finally {
             set({spotifyLoading: false});
         }

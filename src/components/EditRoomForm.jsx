790fc500db681baa7d3f8e6212b24290246739c5
Pinto <pintokevin2002@hotmail.com>
2025-11-21 10:56:40 +0100

    Creating the form to edit a room

---
+++ src/components/EditRoomForm.jsx
@@ -0,0 +1,92 @@
+import {useEffect, useState} from "react";
+import {useSpotifyStore} from "../stores/spotifyStore.js";
+import {Alert, Button, Input, Switch} from "@material-tailwind/react";
+import SelectPlaylistWrapper from "./SelectPlaylistWrapper.jsx";
+import NProgress from "nprogress";
+
+const EditRoomForm = ({room, userToken, userInfo, roomError, editRoom}) => {
+    const [name, setName] = useState("");
+    const [playlistId, setPlaylistId] = useState("");
+    const [isActive, setIsActive] = useState(true);
+    const [error, setError] = useState("");
+
+    const {spotifyError, spotifyLoading, userPlaylists, getUserPlaylist} = useSpotifyStore();
+
+    useEffect(() => {
+        if (userInfo && userToken && userInfo.spotifyId !== "") {
+            getUserPlaylist(userToken);
+        }
+    }, [userInfo, userToken, getUserPlaylist]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const n = name.trim();
+        if (n === "" || n.length < 3 || n.length > 50) {
+            setError("The name must be between 3 and 50 characters.");
+            return;
+        }
+
+        NProgress.start();
+        await editRoom({
+            name: n,
+            isActive: isActive,
+            playlistId: playlistId,
+        }, room.id, userToken);
+        NProgress.done();
+    }
+
+    return (
+        <div>
+            <form className="flex flex-col gap-6 flex-1" onSubmit={handleSubmit}>
+                {roomError ? (
+                    <div className="flex justify-center items-center w-full">
+                        <Alert color="red">
+                            {roomError}
+                        </Alert>
+                    </div>
+                ) : null}
+
+                {error ? (
+                    <div className="flex justify-center items-center w-full">
+                        <Alert color="red">
+                            {error}
+                        </Alert>
+                    </div>
+                ) : null}
+
+                <Input
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    label="Room name"
+                />
+
+                <Switch
+                    checked={isActive}
+                    onChange={() => setIsActive(!isActive)}
+                    label="Is Active"
+                    color="green"
+                />
+
+                <SelectPlaylistWrapper
+                    userInfo={userInfo}
+                    value={playlistId}
+                    error={spotifyError}
+                    setValue={setPlaylistId}
+                    playlists={userPlaylists}
+                    loading={spotifyLoading}
+                />
+
+                <Button
+                    color="green"
+                    type="submit"
+                    onClick={handleSubmit}
+                >
+                    Edit The Room
+                </Button>
+            </form>
+        </div>
+    );
+};
+
+export default EditRoomForm;

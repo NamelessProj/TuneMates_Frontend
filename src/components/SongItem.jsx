e0bfd3a0cf8eb457353c25121d2cd840d30a2bf9
Nameless <pintokevin2002@hotmail.com>
2025-11-24 15:43:54 +0100

    Add background color to SongItem component for improved visibility

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -5,7 +5,7 @@ const SongItem = ({song, calcDuration, handleSendSongToRoom=null, handleAddSongT
         <Card
             color="transparent"
             shadow={false}
-            className="w-full max-w-[26rem]"
+            className="w-full max-w-[26rem] bg-gray-500"
         >
             <CardHeader
                 color="transparent"

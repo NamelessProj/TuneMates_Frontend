3106068893c0bc888d1cf327b43cec4481ea9eaf
Nameless <pintokevin2002@hotmail.com>
2025-11-24 15:38:08 +0100

    Add console log for response data in SearchSongs component

--- src/components/SearchSongs.jsx
+++ src/components/SearchSongs.jsx
@@ -35,6 +35,7 @@ const SearchSongs = ({roomId}) => {
                 withCredentials: true,
                 method: "GET"
             });
+            console.log(res.data);
             results = res.data;
         } catch (err) {
             setSongsError(err?.response?.data?.message || err?.message || "Failed to search songs on Spotify");

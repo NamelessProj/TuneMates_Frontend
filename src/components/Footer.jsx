8f42d84cae1843a3c3569644a77e5522fd24d810
Pinto <pintokevin2002@hotmail.com>
2025-11-28 12:17:57 +0100

    Making sure the footer text is centered

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import {Typography} from "@material-tailwind/react";
 const Footer = () => {
     return (
         <footer>
-            <Typography variant="small" className="text-center font-normal text-primary-white">
+            <Typography variant="small" className="text-center text-balance font-normal text-primary-white">
                 &copy; {new Date().getFullYear()} TuneMates. All rights reserved.
             </Typography>
         </footer>

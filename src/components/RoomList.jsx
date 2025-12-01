543ceea0c5e04652240a6471b36e41ebbba1cc8f
Nameless <pintokevin2002@hotmail.com>
2025-12-01 15:17:00 +0100

    Add full width styling to RoomList component

--- src/components/RoomList.jsx
+++ src/components/RoomList.jsx
@@ -3,7 +3,7 @@ import RoomListEl from "./RoomListEl.jsx";
 
 const RoomList = ({rooms, token}) => {
     return (
-        <div>
+        <div className="w-full">
             {(rooms && rooms.length) ? (
                 rooms.map((room) => (
                     <RoomListEl room={room} token={token} key={room.id} />

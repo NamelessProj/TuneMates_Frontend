17ab971063098a6eacc95b2a691cac26e7732f00
Nameless <pintokevin2002@hotmail.com>
2025-12-01 15:21:42 +0100

    Enhance RoomList layout with vertical alignment and spacing adjustments

--- src/components/RoomList.jsx
+++ src/components/RoomList.jsx
@@ -3,7 +3,7 @@ import RoomListEl from "./RoomListEl.jsx";
 
 const RoomList = ({rooms, token}) => {
     return (
-        <div className="w-full flex justify-center">
+        <div className="w-full flex justify-center items-center flex-col gap-6">
             {(rooms && rooms.length) ? (
                 rooms.map((room) => (
                     <RoomListEl room={room} token={token} key={room.id} />

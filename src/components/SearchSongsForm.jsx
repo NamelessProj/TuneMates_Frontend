31885aa4d4332e3f5b22392c7945207b007e1c32
Nameless <pintokevin2002@hotmail.com>
2025-12-04 15:09:40 +0100

    Center error alert and adjust form width in SearchSongsForm component

--- src/components/SearchSongsForm.jsx
+++ src/components/SearchSongsForm.jsx
@@ -3,9 +3,12 @@ import {Alert, Button, Input} from "@material-tailwind/react";
 const SearchSongsForm = ({input, setInput, error, handleSearch}) => {
     return (
         <div>
-            {error && <Alert color="red" className="w-fit mx-auto my-3">{error}</Alert>}
+            <div className="flex justify-center">
+                {error && <Alert color="red" className="w-fit mx-auto my-3">{error}</Alert>}
+            </div>
+
             <form
-                className="flex flex-col md:flex-row gap-4 md:items-end md:gap-2"
+                className="flex flex-col md:flex-row gap-4 md:items-end md:gap-2 w-full max-w-[36rem] mx-auto"
                 onSubmit={handleSearch}
             >
                 <Input

0db078511935f2401073017c092dd69f6c7da291
Nameless <pintokevin2002@hotmail.com>
2025-12-08 16:50:42 +0100

    Handle undefined playlist properties in SelectPlaylist component

--- src/components/SelectPlaylist.jsx
+++ src/components/SelectPlaylist.jsx
@@ -11,17 +11,17 @@ const SelectPlaylist = ({playlists, value, setValue}) => {
             >
                 {(playlists || []).map((playlist, i) => (
                     <Option
-                        value={playlist.id}
+                        value={playlist?.id || ""}
                         key={i}
                         className="flex items-center gap-2"
                     >
                         <Avatar
-                            src={playlist.imageUrl}
-                            alt={playlist.name}
+                            src={playlist?.imageUrl || "/default_song.png"}
+                            alt={playlist?.name || "Playlist Image"}
                             variant="rounded"
                             className="h-5 w-5 object-cover"
                         />
-                        {playlist.name}
+                        {playlist?.name || "Unnamed Playlist"} {playlist?.id || "ID"}
                     </Option>
                 ))}
             </Select>

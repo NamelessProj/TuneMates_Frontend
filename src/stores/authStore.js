2bcc618a28188bbf2cfaf771b14cad3a0ec75050
Nameless <pintokevin2002@hotmail.com>
2025-12-10 01:42:30 +0100

    Add JSDoc comments for user credential management methods in authStore

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -9,11 +9,21 @@ export const useAuthStore = create((set) => ({
     userToken: localStorage.getItem(tokenStorageKey) ? JSON.parse(localStorage.getItem(tokenStorageKey)) : null,
     userTokenExpiresAt: localStorage.getItem(tokenStorageExpiresAtKey) ? JSON.parse(localStorage.getItem(tokenStorageExpiresAtKey)) : null,
 
+    /**
+     * Sets the user credentials both in the store and in localStorage.
+     * @param data {object} The user information to store
+     * @returns {void}
+     */
     setCredentials: (data) => {
         set(() => ({userInfo: data}));
         localStorage.setItem(storageKey, JSON.stringify(data));
     },
 
+    /**
+     * Sets the user token both in the store and in localStorage.
+     * @param token {string} The user token to store
+     * @returns {void}
+     */
     setUserToken: (token) => {
         const expiresAt = Date.now() + 60 * 60 * 1000; // 1 hour from now
         set(() => ({userToken: token, userTokenExpiresAt: expiresAt}));
@@ -21,6 +31,10 @@ export const useAuthStore = create((set) => ({
         localStorage.setItem(tokenStorageExpiresAtKey, JSON.stringify(expiresAt));
     },
 
+    /**
+     * Logs out the user by clearing the store and localStorage.
+     * @returns {void}
+     */
     logout: () => {
         set(() => ({userInfo: null}));
         set(() => ({userToken: null}));

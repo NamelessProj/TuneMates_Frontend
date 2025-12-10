bb3be07553da44408552d66b44f20d9ccef64330
Nameless <pintokevin2002@hotmail.com>
2025-12-10 01:25:25 +0100

    Add JSDoc comments for validateEmail function

--- src/utils/validateEmail.js
+++ src/utils/validateEmail.js
@@ -1,3 +1,8 @@
+/**
+ * Validates if the given string is a valid email format.
+ * @param email {string} The email string to validate
+ * @returns {boolean} `true` if valid email, `false` otherwise
+ */
 const validateEmail = (email) => {
     const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     return regex.test(email);

0fd6373116f5b19c73466a386eded8bc85d5f139
Nameless <pintokevin2002@hotmail.com>
2025-12-10 01:25:15 +0100

    Add JSDoc comments for focusById function

--- src/utils/focusById.js
+++ src/utils/focusById.js
@@ -1,3 +1,8 @@
+/**
+ * Focuses an element by its ID.
+ * @param id {string} The ID of the element to focus
+ * @returns {void | undefined}
+ */
 const focusById = (id) => document.getElementById(id)?.focus();
 
 export default focusById;

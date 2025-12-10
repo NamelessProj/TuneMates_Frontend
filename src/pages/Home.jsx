b72290a1beaae79e2ba7c4e1501f21153fac9fab
Nameless <pintokevin2002@hotmail.com>
2025-12-10 15:26:41 +0100

    Add link to join current room with accessibility improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import {useRoomStore} from "../stores/roomStore.js";
 import NProgress from "nprogress";
 import DefaultSpinner from "../components/DefaultSpinner.jsx";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import PasswordInput from "../components/PasswordInput.jsx";
 import focusById from "../utils/focusById.js";
 
@@ -106,6 +106,35 @@ const Home = () => {
                     </CardBody>
                 </Card>
             )}
+
+            {currentRoom ? (
+                <Card color="gray">
+                    <CardBody className="grid grid-cols-1 gap-6 md:grid-cols-[1fr_auto] items-center justify-between">
+                        <div>
+                            <Typography variant="h4">
+                                Join Back
+                            </Typography>
+                            <Typography variant="lead" color="green">
+                                {currentRoom.name}
+                            </Typography>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <Link to={`/room/slug/${currentRoom.slug}`}>
+                                <Button
+                                    variant="text"
+                                    color="green"
+                                    className="fill-green-300 hover:fill-green-500"
+                                >
+                                    <p className="sr-only">Click here to join this room</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 640 640">
+                                        <path d="M566.6 342.6C579.1 330.1 579.1 309.8 566.6 297.3L406.6 137.3C394.1 124.8 373.8 124.8 361.3 137.3C348.8 149.8 348.8 170.1 361.3 182.6L466.7 288L96 288C78.3 288 64 302.3 64 320C64 337.7 78.3 352 96 352L466.7 352L361.3 457.4C348.8 469.9 348.8 490.2 361.3 502.7C373.8 515.2 394.1 515.2 406.6 502.7L566.6 342.7z"/>
+                                    </svg>
+                                </Button>
+                            </Link>
+                        </div>
+                    </CardBody>
+                </Card>
+            ) : null}
         </main>
     );
 };

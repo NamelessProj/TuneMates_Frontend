fb7b068711ddc5232854c4e0f756f91c21cc61a9
Nameless <pintokevin2002@hotmail.com>
2025-12-10 01:25:20 +0100

    Add JSDoc comments for parseMsToMinutesSeconds function

--- src/utils/parseMsToMinutesSeconds.js
+++ src/utils/parseMsToMinutesSeconds.js
@@ -1,3 +1,8 @@
+/**
+ * Parse milliseconds to minutes and seconds format (M:SS)
+ * @param ms {number} The time in milliseconds
+ * @returns {string} The time in M:SS format
+ */
 const parseMsToMinutesSeconds = (ms) => {
     const minutes = Math.floor(ms / 60000);
     const seconds = Math.floor((ms % 60000) / 1000);

2dc5724bc3bf32421d1733a1d8e63105f428b12d
Nameless <pintokevin2002@hotmail.com>
2025-12-10 15:54:05 +0100

    Refactor SearchSongsForm layout for improved styling and alignment

--- src/components/SearchSongsForm.jsx
+++ src/components/SearchSongsForm.jsx
@@ -9,19 +9,21 @@ const SearchSongsForm = ({input, setInput, error, handleSearch}) => {
             </div>
 
             <form
-                className="flex flex-col md:flex-row gap-4 md:items-end md:gap-2 w-full max-w-[36rem] mx-auto"
+                className="flex flex-col md:flex-row gap-0 md:items-end w-full max-w-[36rem] mx-auto"
                 onSubmit={handleSearch}
             >
                 <Input
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
                     label="Search Songs"
+                    className="rounded-b-none md:rounded-bl-[7px] md:rounded-tr-none"
                 />
                 <Button
                     type="submit"
                     color="green"
                     variant="gradient"
                     onClick={handleSearch}
+                    className="rounded-t-none md:rounded-tr-[7px] md:rounded-bl-none"
                 >
                     Search
                 </Button>

462358fd020189bfcc06cfd27c5846a51283acc8
Nameless <pintokevin2002@hotmail.com>
2025-12-11 15:20:49 +0100

    Refactor OwnerSongList component to accept hasPlaylist prop for SongItem

--- src/components/OwnerSongList.jsx
+++ src/components/OwnerSongList.jsx
@@ -2,13 +2,14 @@ import SongItem from "./SongItem.jsx";
 import parseMsToMinutesSeconds from "../utils/parseMsToMinutesSeconds.js";
 import {Typography} from "@material-tailwind/react";
 
-const OwnerSongList = ({songs, handleAddSongToPlaylist}) => {
+const OwnerSongList = ({songs, hasPlaylist, handleAddSongToPlaylist}) => {
     return (
         <div className="flex flex-col justify-center items-center my-6">
             {songs.length > 0 ? (
                 songs.map((song, i) => <SongItem
                     key={i}
                     song={song}
+                    hasPlaylist={hasPlaylist}
                     calcDuration={parseMsToMinutesSeconds}
                     handleAddSongToPlaylist={handleAddSongToPlaylist}
                 />)
